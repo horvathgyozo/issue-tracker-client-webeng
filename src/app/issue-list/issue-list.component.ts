5b978fff1ec107327eca45248b3153763d5fcd8a
Győző Horváth <gyozo.horvath@inf.elte.hu>
2019-04-02 16:09:05 +0200

    dynamic list

--- src/app/issue-list/issue-list.component.ts
+++ src/app/issue-list/issue-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Issue } from '../issue';
 
 @Component({
   selector: 'app-issue-list',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./issue-list.component.css']
 })
 export class IssueListComponent implements OnInit {
+
+  issues: Issue[] = [
+    { id: 1, title: 'issue1', description: 'desc1', place: 'place1', status: 'NEW' },
+    { id: 2, title: 'issue2', description: 'desc2', place: 'place2', status: 'DOING' },
+    { id: 3, title: 'issue3', description: 'desc3', place: 'place3', status: 'DONE' },
+    { id: 4, title: 'issue4', description: 'desc4', place: 'place4', status: 'DOING' },
+  ];
 
   constructor() { }
 
