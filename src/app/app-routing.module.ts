6687cb5d3016197ba55ceb1cdf53f016c55b1edd
Győző Horváth <gyozo.horvath@inf.elte.hu>
2019-04-02 09:56:25 +0200

    html mockups

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: '',
+  //   component: MainPageComponent
+  // },
+  // {
+  //   path: 'issues',
+  //   component: IssueListComponent
+  // },
+  // {
+  //   path: 'issues/new',
+  //   component: IssueFormComponent
+  // },
+  // {
+  //   path: 'issues/:id',
+  //   component: IssueDetailComponent
+  // },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
